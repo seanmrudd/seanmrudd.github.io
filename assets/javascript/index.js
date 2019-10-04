776074456aa3839f9911909d81ff92474635acc2
shrudder <seanmrudd@me.com>
2019-10-03 20:34:40 -0400

    commit

---
+++ assets/javascript/index.js
@@ -0,0 +1,17 @@
+// let imgArray = ["assets/images/profile.jpg", "assets/images/credly-badge.png"]
+
+// let imgOne = $("<img>");
+// imgOne.attr("id", "myPic");
+// imgOne.attr("src", imgArray[0]);
+// imgOne.attr("alt", "Profile Pic");
+
+// $("#pic1").append(imgOne);
+
+// console.log("hi")
+
+// let imgTwo = $("<img>");
+// imgTwo.attr("id", "credlyBadge");
+// imgTwo.attr("src", imgArray[1]);
+// imgTwo.attr("alt", "Badge");
+
+// $("#pic2").append(imgTwo);
